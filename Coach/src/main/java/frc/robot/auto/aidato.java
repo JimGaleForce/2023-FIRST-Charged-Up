01ed93ada702e433ab62ab58ce0288144a6de8d2
aiibs <aimadhay@am-hs.org>
2023-02-04 09:33:25 -0800

    messed

--- Coach/src/main/java/frc/robot/auto/aidato.java
+++ Coach/src/main/java/frc/robot/auto/aidato.java
@@ -33,23 +33,17 @@ public class aidato implements AutoRoutine {
 
 
 
-              new DriveTimeCmd(this.c_Drive, 0.5, 0, 1000);
+              new DriveTimeCmd(this.c_Drive, 0.5, 0, 1000),
 
-              new TurnCmd(this.c_Drive, this.c_Gyro, 90, 4000);
+              new TurnCmd(this.c_Drive, this.c_Gyro, 90, 4000),
   
-              new DriveTimeCmd(this.c_Drive, 0.5 , 0, 2000);
+              new DriveTimeCmd(this.c_Drive, 0.5, 0, 2000),
 
-              new TurnCmd(this.c_Drive, this.c_Gyro, -90, 4000);
+              new TurnCmd(this.c_Drive, this.c_Gyro, -90, 4000),
 
-              new DriveTimeCmd(this.c_Drive, 0.7, 0, 2000);
-
-              new DriveTimeCmd(this.c_Drive, 0., 0, 3000);
+              new DriveTimeCmd(this.c_Drive, 0.6, 0, 2000),
   
-              new DriveTimeCmd(this.c_Drive, 0, 0, 0);
-
-
-
- 
+              new DriveTimeCmd(this.c_Drive, 0, 0, 0)
 
           ));
   
